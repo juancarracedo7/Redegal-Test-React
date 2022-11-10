4c7d7b1ed15725ae90fd7aa555c3496de2cdce04
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-11-10 15:23:32 -0300

    App routing

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import Details from "./components/Details";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route axact path="/details/:id" element={<Details />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;

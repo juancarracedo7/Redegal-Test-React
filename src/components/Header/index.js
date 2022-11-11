6229e49715ab409ef226bd3b0b7961e947ec1785
Juan Cruz Carracedo <juann.carracedo7@gmail.com>
2022-11-11 05:52:27 -0300

    Local storage and number cart render

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,42 @@
-import { Link } from "react-router-dom";
-import { BsCart3 } from 'react-icons/bs';
+import { Link, NavLink } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import styles from "./Header.module.css";
+import { useEffect } from "react";
+
 
 export default function Header() {
-  return (
-    <div>
-      <Link to="/">Logo</Link>
-      <Link to="/">Home -- </Link>
-      <Link to="/details/:id">Details</Link>
-      <div> <BsCart3></BsCart3></div>
+  const cartNumber = useSelector((state) => state.cart);
 
+  let deactivatedStyle = {
+    display: "none",
+  };
+
+
+  const cartNumberLocal = JSON.parse(localStorage.getItem("cartNumber"))
+  console.log("cartNumberLocal", cartNumberLocal);
+
+  return (
+    <>
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to="/">Market Place</Link>
+      </div>
+      <div>
+        {" "}
+        <BsCart3></BsCart3> { cartNumberLocal  || cartNumber} items
+      </div>
     </div>
+      <div className={styles.breadcrumbs}>
+      <NavLink to="/">Home &nbsp; </NavLink>
+      <NavLink
+      onClick={(e) => {e.preventDefault()}}
+        to="/details"
+        style={({ isActive }) => (isActive ? undefined : deactivatedStyle)}
+      >
+         / Details
+      </NavLink>
+      </div>
+    </>
   );
 }
